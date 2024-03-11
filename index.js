2bf42efef5b3e853fbac30257daf71ee10a62683
2883857@student.edu.az <2883857@student.edu.az>
2024-03-12 01:53:44 +0400

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 
@@ -9,6 +10,8 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+
+app.use(cors())
 app.use(express.json())
 
 const jokesRouter = require("./routes/jokes")
